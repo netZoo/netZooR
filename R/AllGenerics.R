51305545afa5ed0f26fa7169e5f6f649d890e965
d.schlauch <d.schlauch@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-04-09 00:53:29 +0000

    setting up git-svn bridge
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pandaR@102143 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -12,10 +12,13 @@
 #' data(pandaToyData)
 #' panda.res <- panda(pandaToyData$motif,
 #'            pandaToyData$expression,pandaToyData$ppi,hamming=.001,progress=TRUE)
-#' summary(panda.res)}
+#' summary(panda.res)
+#' }
+#' data(pandaResult)
+#' summary(pandaResult)
 summary.panda <- function(object, ...){
     l <- list(coregNet=dim(object@coregNet),regNet=dim(object@regNet),coopNet=dim(object@coopNet))
-    message("PANDA network for", nrow(object@coregNet),"genes and",nrow(object@coopNet)," transcription factors.")
+    message("PANDA network for ", nrow(object@coregNet)," genes and ",nrow(object@coopNet)," transcription factors.")
 }
 #' print.panda
 #'
@@ -31,16 +34,19 @@ summary.panda <- function(object, ...){
 #' data(pandaToyData)
 #' panda.res <- panda(pandaToyData$motif,
 #'            pandaToyData$expression,pandaToyData$ppi,hamming=.001,progress=TRUE)
-#' print(panda.res)}
+#' print(panda.res)
+#' }
+#' data(pandaResult)
+#' print(pandaResult)
 print.panda <- function(x, ...){
     l <- list(coregNet=dim(x@coregNet),regNet=dim(x@regNet),coopNet=dim(x@coopNet))
     message("PANDA network for", nrow(x@coregNet),"genes and",nrow(x@coopNet),"transcription factors.")
     message("\nSlots:")
-    message(slotNames(x)[1],"\t: Regulatory network of",nrow(x@coopNet)," transcription factors to", nrow(x@coregNet),"genes.")
-    message(slotNames(x)[2],": Co-regulation network of", nrow(x@coregNet),"genes.")
-    message(slotNames(x)[3],"\t: Cooperative network of", nrow(x@coopNet),"transcription factors.\n")
+    message(slotNames(x)[1],"\t: Regulatory network of ",nrow(x@coopNet)," transcription factors to ", nrow(x@coregNet)," genes.")
+    message(slotNames(x)[2],": Co-regulation network of ", nrow(x@coregNet)," genes.")
+    message(slotNames(x)[3],"\t: Cooperative network of ", nrow(x@coopNet)," transcription factors.\n")
     numEdges <- sum(x@regNet!=0)
-    message("Regulatory graph contains ",numEdges,"edges.")
+    message("Regulatory graph contains ",numEdges," edges.")
     if (numEdges==nrow(x@regNet)*ncol(x@regNet)){
         message("Regulatory graph is complete.")
     } else {
@@ -63,8 +69,10 @@ print.panda <- function(x, ...){
 #'            pandaToyData$expression,pandaToyData$ppi,hamming=.001,progress=TRUE)
 #' plot(panda.res)
 #' }
+#' data(pandaResult)
+#' plot(pandaResult)
 plot.panda <- function(x, ...){
-    message("PANDA network for", nrow(x@coregNet),"genes and",nrow(x@coopNet)," transcription factors.")
+    message("PANDA network for ", nrow(x@coregNet)," genes and ",nrow(x@coopNet)," transcription factors.")
     message("Mean edge weight = ", mean(x@regNet))
     message("Min edge weight = ", min(x@regNet))
     message("Max edge weight = ", max(x@regNet))
