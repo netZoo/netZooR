8d8dd2deb3ae882001b99faf8878287e3f1221d8
Albert Young <albert@alberts-mbp-2.wireless.dartmouth.edu>
2015-09-28 22:42:47 -0400

    changed vignette back to lowercase

--- inst/doc/condor.R
+++ inst/doc/condor.R
@@ -1,14 +1,14 @@
-### R code from vignette source 'CONDOR.Rnw'
+### R code from vignette source 'condor.Rnw'
 
 ###################################################
-### code chunk number 1: CONDOR.Rnw:31-33
+### code chunk number 1: condor.Rnw:31-33
 ###################################################
 library(condor)
 library(igraph)
 
 
 ###################################################
-### code chunk number 2: CONDOR.Rnw:36-41
+### code chunk number 2: condor.Rnw:36-41
 ###################################################
 r = c(1,1,1,2,2,2,3,3,3,4,4);
 b = c(1,2,3,1,2,4,2,3,4,3,4);
@@ -18,19 +18,19 @@ elist <- data.frame(red=reds[r], blue=blues[b])
 
 
 ###################################################
-### code chunk number 3: CONDOR.Rnw:44-45
+### code chunk number 3: condor.Rnw:44-45
 ###################################################
 condor.object <- create.condor.object(elist)
 
 
 ###################################################
-### code chunk number 4: CONDOR.Rnw:48-49
+### code chunk number 4: condor.Rnw:48-49
 ###################################################
 names(condor.object)
 
 
 ###################################################
-### code chunk number 5: CONDOR.Rnw:52-55
+### code chunk number 5: condor.Rnw:52-55
 ###################################################
 condor.object <- condor.cluster(condor.object)
 print(condor.object$red.memb)
@@ -38,7 +38,7 @@ print(condor.object$blue.memb)
 
 
 ###################################################
-### code chunk number 6: CONDOR.Rnw:59-62
+### code chunk number 6: condor.Rnw:59-62
 ###################################################
 gtoy = graph.edgelist(as.matrix(elist),directed=FALSE)
 set.graph.attribute(gtoy, "layout", layout.kamada.kawai(gtoy))
@@ -46,19 +46,19 @@ V(gtoy)[c(reds,blues)]$color <- c(rep("red",4),rep("blue",4))
 
 
 ###################################################
-### code chunk number 7: CONDOR.Rnw:65-66
+### code chunk number 7: condor.Rnw:65-66
 ###################################################
 plot(gtoy,vertex.label.dist=2)
 
 
 ###################################################
-### code chunk number 8: CONDOR.Rnw:70-71
+### code chunk number 8: condor.Rnw:70-71
 ###################################################
 condor.object <- condor.qscore(condor.object)
 
 
 ###################################################
-### code chunk number 9: CONDOR.Rnw:75-78
+### code chunk number 9: condor.Rnw:75-78
 ###################################################
 q_women <- condor.object$qscores$red.qscore
 core_stats <- condor.core.enrich(test_nodes=c("Alice","Mary"),
@@ -66,7 +66,7 @@ core_stats <- condor.core.enrich(test_nodes=c("Alice","Mary"),
 
 
 ###################################################
-### code chunk number 10: CONDOR.Rnw:82-85
+### code chunk number 10: condor.Rnw:82-85
 ###################################################
 data(small1976)
 condor.object <- create.condor.object(small1976)
@@ -74,7 +74,7 @@ condor.object <- condor.cluster(condor.object, project=F)
 
 
 ###################################################
-### code chunk number 11: CONDOR.Rnw:88-89
+### code chunk number 11: condor.Rnw:88-89
 ###################################################
 condor.plot.heatmap(condor.object)
 
