3389b1b4e6356b7bd75df50ddf40685380e558bf
John Platig <jplatig@jimmy.harvard.edu>
2015-10-09 14:18:30 -0400

    Updated documentation.

---
+++ R/data.documentation.R
@@ -0,0 +1,17 @@
+#' Pollinator-plant interactions
+#'
+#' A dataset containing the number of interactions 34 plants and 13 pollinators. The variables are as follows:
+#'
+#' \itemize{
+#'   \item pollinator. Species name of insect pollinator
+#'   \item plant. Species name of plant
+#'   \item interactions. Number of visitors caught on each plant species
+#' }
+#'
+#' @docType data
+#' @references \url{https://www.nceas.ucsb.edu/interactionweb/html/small_1976.html}
+#' @keywords datasets
+#' @name small1976
+#' @usage data(small1976)
+#' @format A data frame with 442 rows and 3 variables
+NULL
