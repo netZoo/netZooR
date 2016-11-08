9ed957f6949bf525de8212f87b4f5d6b811a487e
choyichen <ntu.joey@gmail.com>
2016-11-08 13:23:25 -0500

    add new function to helper.R

--- R/helper.R
+++ R/helper.R
@@ -31,3 +31,29 @@ importPandaMatlab<-function(dir=getwd(),celldata="celldata.dat"){
   colnames(pandaFiles)[1:2] <- c("regulator", "target") # regulator and target columns
   pandaFiles
 }
+
+#' Network exporter
+#'
+#' exportNetwork writes the panda network (RegNet) to foreign file formats.
+#'
+#' @param x An object of class "panda" or an incidence network matrix.
+#' @param file A connection or a string giving the file name to output the network.
+#' @param format Character string giving the file format supported by igraph.
+#' @keywords keywords
+#' @importFrom igraph graph.incidence
+#' @importFrom igraph write.graph
+#' @export
+#' @examples
+#' \donttest{
+#' data(pandaToyData)
+#' pandaRes <- panda(pandaToyData$motif, pandaToyData$expression, pandaToyData$ppi)
+#' topPandaRes <- topedges(pandaRes, 1000)
+#' exportNetwork(topPandaRes, 'topPandaRes.txt')
+#' }
+exportNetwork <- function(x, file, format='ncol') {
+    if (class(x) == "panda") {
+        x <- x@regNet
+    }
+    g <- graph.incidence(x)
+    write.graph(g, file, format)
+}
