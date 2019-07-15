d14a6b7d15e6b79cfd1f02414a7438b43e596efc
TianWang <tianwangxxx@gmail.com>
2019-07-15 17:28:30 -0400

    add Cytoscape

---
+++ installCytoscape.sh
@@ -0,0 +1,5 @@
+brew install wget
+wget https://github.com/cytoscape/cytoscape/releases/download/3.7.1/Cytoscape_3_7_1_macos.dmg
+sudo hdiutil attach Cytoscape_3_7_1_macos.dmg
+sudo installer -package /Volumes/Cytoscape_3_7_1_macos/Cytoscape_3_7_1_macos.pkg -target /
+
