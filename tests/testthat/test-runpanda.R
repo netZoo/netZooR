8a61c645091c19dbc4609a4d21b680b2b828d55d
TianWang <tianwangxxx@gmail.com>
2019-08-16 14:17:54 -0400

    add test-runpanda.r

--- tests/testthat/test-runpanda.R
+++ tests/testthat/test-runpanda.R
@@ -3,9 +3,16 @@ context("test-runpanda")
 test_that("runPanda function works", {
   
   expect_error(runPanda())
-  treated_expression_file_path <- system.file("extdata", "expr4_matched.txt", package = "netZooR", mustWork = TRUE)
+  T4_expression_file_path <- system.file("extdata", "expr4_matched.txt", package = "netZooR", mustWork = TRUE)
+  T10_expression_file_path <- system.file("extdata", "expr10_matched.txt", package = "netZooR", mustWork = TRUE)
   motif_file_path <- system.file("extdata", "chip_matched.txt", package = "netZooR", mustWork = TRUE)
   ppi_file_path <- system.file("extdata", "ppi_matched.txt", package = "netZooR", mustWork = TRUE)
-  expect_message(testPanda1 <- runPanda(treated_expression_file_path))
-  expect_message(testPanda2 <- runPanda(treated_expression_file_path,motif_file_path,rm_missing = TRUE),"")
+  expect_message(testPanda1 <- runPanda(T4_expression_file_path))
+  expect_message(testPanda2 <- runPanda(T4_expression_file_path,motif_file_path,rm_missing = TRUE),"")
+  actual_T4pandaNet <- runPanda(T4_expression_file_path, motif_file_path,ppi_file_path)$panda
+  expected_T4pandaNet <- read.csv("./testdata/expr4_PandaNet.txt",sep = " ",header = F)
+  expect_equal(sum(abs(actual_T4pandaNet[,4]-expected_T4pandaNet[,4])),0)
 })
+
+
+
