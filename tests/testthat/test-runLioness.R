b2d6c45de8572453411b318ea2cc4961cb650ea7
TianWang <tianwangxxx@gmail.com>
2019-08-23 16:15:28 -0400

    add arguments in runLioness()

--- tests/testthat/test-runLioness.R
+++ tests/testthat/test-runLioness.R
@@ -14,10 +14,10 @@ test_that("runLioness function works", {
   ppi_file_path <- system.file("extdata", "ppi_matched.txt", package = "netZooR", mustWork = TRUE)
   
   # test message when PPI is not provided
-  expect_message(testPanda2 <- runLioness(T4_expression_file_path,motif_file_path,rm_missing = TRUE),"")
+  expect_message(testPanda2 <- runLioness(T4_expression_file_path,motif_file_path,rm_missing = TRUE, start_sample=1, end_sample=1),"")
   
-  # run LIONESS with T4 dataset
-  actual_T4lioness<- runLioness(T4_expression_file_path, motif_file_path,ppi_file_path)
+  # run LIONESS with T4 dataset for only sample 1s
+  actual_T4lioness<- runLioness(T4_expression_file_path, motif_file_path,ppi_file_path, rm_missing = TRUE,start_sample=1, end_sample=1)
   
   # Lioness sample 1 network compare
   expect_equal(sum(abs(actual_T4lioness[,3]-expected_T4lioness_samp1_list$T4_Samp1)),0)
