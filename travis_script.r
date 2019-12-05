2e3a7024a264e4c75423737acc283a7ea06c21dc
marouenbg <marouen.b.guebila@gmail.com>
2019-12-05 17:25:09 -0500

    fix rtavis

--- travis_script.r
+++ travis_script.r
@@ -1,8 +1,5 @@
 install.packages("devtools")
-install.packages("BiocInstaller")
 library(devtools)
-library(BiocInstaller)
 
-biocValid(fix=TRUE) 
 update.packages(ask = FALSE, checkBuilt = TRUE)
 devtools::install_deps(dep = T)
