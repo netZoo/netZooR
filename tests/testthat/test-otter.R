3ec279dfbdd0e8e3851bf36229690c6437721c50
marouenbg <marouen.b.guebila@gmail.com>
2020-04-30 05:31:24 -0400

    decrease iter to 1

--- tests/testthat/test-otter.R
+++ tests/testthat/test-otter.R
@@ -7,6 +7,6 @@ test_that("otter function works", {
    gt=as.matrix(read.csv('./test_otter.csv', header = FALSE))
 
    # Run OTTER algorithm
-   W <- otter(W, P, C, Iter = 10)
+   W <- otter(W, P, C, Iter = 1)
    assert_that((all((as.integer(W*10**2)/10**2) == (as.integer(gt*10**2)/10**2))))  
 })
