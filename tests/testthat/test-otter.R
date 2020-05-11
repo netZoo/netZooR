4115d8255dc54f8ac00d653ccfcad1bf0df7feae
TianWang <tianwangxxx@gmail.com>
2020-05-11 19:56:52 -0400

    add otter test datat to testDataset.RData

--- tests/testthat/test-otter.R
+++ tests/testthat/test-otter.R
@@ -1,12 +1,10 @@
 context("test OTTER result")
 
 test_that("otter function works", {
-   W=as.matrix(read.csv('./w.csv', header = FALSE))
-   C=as.matrix(read.csv('./c.csv', header = FALSE))
-   P=as.matrix(read.csv('./p.csv', header = FALSE))
-   gt=as.matrix(read.csv('./test_otter.csv', header = FALSE))
-
+   load("./testDataset.RData")
+   
    # Run OTTER algorithm
-   W <- otter(W, P, C, Iter = 1)
-   assert_that((all((as.integer(W*10**2)/10**2) == (as.integer(gt*10**2)/10**2))))  
+   otterW <- otter(W, P, C, Iter = 1)
+   expect_equal(as.integer(otterW*10**2)/10**2, as.integer(gt*10**2)/10**2)  
 })
+
