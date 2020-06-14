fda7efcade3b919c19f8560864413d48b66629b3
Marouen <marouenbg@users.noreply.github.com>
2020-06-13 23:46:19 -0400

    Update zookeeper.sh

--- zookeeper.sh
+++ zookeeper.sh
@@ -3,3 +3,4 @@ pip3 install --user numpy scipy pandas
 Rscript travis_script.r
 R CMD build .
 R CMD check *tar.gz --no-manual
+Rscript -e 'devtools::install_github("r-lib/covr");library(covr);codecov()'
