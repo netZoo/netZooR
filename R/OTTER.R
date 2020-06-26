cbbecd1a134de23dcdf931bd8b393dd392128224
Rebekka Burkholz <rebekka.burkholz@gmail.com>
2020-06-26 12:44:02 -0400

    update to otter2

--- R/OTTER.R
+++ R/OTTER.R
@@ -6,22 +6,20 @@
 #'               minimzing the following objective:
 #'                                  min f(W) 
 #'               with f(W) = (1-lambda)*||WW' - P||^2 + lambda*||W'W - C||^2 + (gamma/2)*||W||^2
-#'               
-#' Authors: 
-#'               Rebekka Burkholz 4/2020               
 #'
-#' @param W TF-gene regulatory network based on TF motifs as a
+#' Inputs:
+#' @param W     : TF-gene regulatory network based on TF motifs as a
 #'                       matrix of size (t,g), g=number of genes, t=number of TFs
-#' @param P TF-TF protein interaction network as a matrix of size (t,t)
-#' @param C gene coexpression as a matrix of size (g,g) 
-#' @param lambda tuning parameter in [0,1] (higher gives more weight to C)
-#' @param gamma regularization parameter
-#' @param Iter number of iterations of the algorithm
-#' @param eta learning rate
-#' @param bexp exponent influencing learning rate (higher means smaller)
+#' @param P     : TF-TF protein interaction network as a matrix of size (t,t)
+#' @param C     : gene coexpression as a matrix of size (g,g) 
+#' @param lambda: tuning parameter in [0,1] (higher gives more weight to C)
+#' @param gamma : regularization parameter
+#' @param Iter  : number of iterations of the algorithm
+#' @param eta   : learning rate
+#' @param bexp  : exponent influencing learning rate (higher means smaller)
 #'
+#' Outputs:
 #' @return W    : Predicted TF-gene complete regulatory network as an adjacency matrix of size (t,g).
-#'
 #'
 #' @examples
 #'
@@ -34,7 +32,7 @@
 #'  
 #' @export
 
-otter <- function(W, P, C, lambda = 0.0035, gamma = 0.335, Iter = 300, eta = 0.00001, bexp = 1){
+otter <- function(W, P, C, lambda = 0.0035, gamma = 0.335, Iter = 32, eta = 0.00001, bexp = 1){
   #ADAM parameters
   b1 <- 0.9
   b2 <- 0.999
@@ -45,23 +43,25 @@ otter <- function(W, P, C, lambda = 0.0035, gamma = 0.335, Iter = 300, eta = 0.0
   dW <- dim(W)
   nTF <- dW[1]
   nGenes <- dW[2]
-  P <- P*((1-lambda)/sum(diag(P))) + (1-lambda)*0.0013
-  C <- C*(lambda/sum(diag(C)))
+  P <- P+2.2
+  P <- P/sum(diag(P))
   W <- P%*%W
-  W <- W/sqrt(sum(diag(W%*%t(W))))
+  P <- P*(1-lambda)
+  C <- C*(lambda/sum(diag(C)))
+  W <- W/sum(diag(W%*%t(W)))
   P = P - gamma*diag(nTF)
   m <- matrix(data = 0, nrow = nTF, ncol = nGenes)
   v <- m 
   for(i in 1:Iter){
     grad <- W%*%t(W)%*%W -P%*%W - W%*%C  
-       m <- b1*m + (4*(1-b1))*grad
-       v <- b2*v + (16*(1-b2))*grad^2 
-      b1t <- b1t*b1
-      b2t <- b2t*b2
-      alpha <- sqrt(1-b2t)/(1-b1t)*eta
-      epst <- eps*sqrt((1-b2t))
-      #update of gene ragulatory matrix
-       W <- W - alpha*(m/(epst+sqrt(v)))
+    m <- b1*m + (4*(1-b1))*grad
+    v <- b2*v + (16*(1-b2))*grad^2 
+    b1t <- b1t*b1
+    b2t <- b2t*b2
+    alpha <- sqrt(1-b2t)/(1-b1t)*eta
+    epst <- eps*sqrt((1-b2t))
+    #update of gene ragulatory matrix
+    W <- W - alpha*(m/(epst+sqrt(v)))
   }
   return(W)
 }
