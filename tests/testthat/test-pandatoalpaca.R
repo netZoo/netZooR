16d10b6b48372ae3360d163bb6cb11246bc0bdbf
TianWang <tianwangxxx@gmail.com>
2020-08-03 15:16:15 -0400

    reduce test size

--- tests/testthat/test-pandatoalpaca.R
+++ tests/testthat/test-pandatoalpaca.R
@@ -2,6 +2,6 @@ context("test panda.to.alpaca function")
 
 test_that(" panda.to.alpaca works or not", {
   load("./testDataset.RData")
-  alpaca <- panda.to.alpaca(T4Panda,T10Panda,NULL,verbose = F)
+  alpaca <- panda.to.alpaca(T4Panda_subset,T10Panda_subset,NULL,verbose = F)
   expect_equal(alpaca, T4T10Alpaca)
 }) 
--- tests/testthat/test-pandatocondorobject.R
+++ tests/testthat/test-pandatocondorobject.R
@@ -2,6 +2,6 @@ context("test panda.to.condor.object function")
 
 test_that(" panda.to.condor.object works or not", {
   load("./testDataset.RData")
-  condor.obj <- panda.to.condor.object(T4Panda)
-  expect_equal(condor.obj$modularity, T4Condor$modularity)
+  condor.obj <- panda.to.condor.object(T4Panda_subset)
+  expect_equal(condor.obj$edges, T4Condor$edges)
 }) 
