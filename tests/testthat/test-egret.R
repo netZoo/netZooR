26fa4a4eb17422fc09a06ee771d3a7f16106ed89
marouenbg <marouen.b.guebila@gmail.com>
2020-11-24 19:37:21 -0500

    update egret

--- tests/testthat/test-egret.R
+++ tests/testthat/test-egret.R
@@ -11,7 +11,7 @@ test_that("EGRET function works", {
   system("curl -O  https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/EGRET/EgretGT.RData")
   
   qbic <- read.table(file = "toy_qbic.txt", header = FALSE)
-  vcf <- read.table("toy_genotype.vcf", header = FALSE, sep = "\t")
+  vcf <- read.table("toy_genotype.vcf", header = FALSE, sep = "\t", stringsAsFactors = FALSE, colClasses = c("character", "numeric", "character", "character", "character", "character", "character", "character", "character", "character"))
   motif <- read.table("toy_motif_prior.txt", sep = "\t", header = FALSE)
   expr <- read.table("toy_expr.txt", header = FALSE, sep = "\t", row.names = 1)
   ppi <- read.table("toy_ppi_prior.txt", header = FALSE, sep = "\t")
