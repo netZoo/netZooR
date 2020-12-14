d404455995113fb9a6b1ae747d40ad35060906b8
marouenbg <marouen.b.guebila@gmail.com>
2020-12-14 16:21:00 -0500

    update otter data set

--- tests/testthat/test-otter.R
+++ tests/testthat/test-otter.R
@@ -4,7 +4,7 @@ test_that("otter function works", {
    load("./testDataset.RData")
    
    # Run OTTER algorithm
-   otterW <- otter(W, P, C, Iter = 1)
+   otterW <- otter(W, P, C, Iter = 1, lambda = 0.0035, gamma = 0.335)
    expect_equal(as.integer(otterW*10**10)/10**10, as.integer(gt*10**10)/10**10)  
 })
 
