c8a8216cab466c8f613ba3f0d149ce36edacf7bd
Marouen <marouenbg@users.noreply.github.com>
2021-08-16 17:36:46 -0400

    provide codecov token directly instead of file

--- coverZoo.r
+++ coverZoo.r
@@ -1,4 +1,3 @@
 library(covr)
-fileName <- '/home/ubuntu/token.txt'
-token=readChar(fileName, file.info(fileName)$size);token=gsub("[\r\n]", "", token)
+token='eb271df0-06bb-49bb-9b4a-2d250baf2328'
 codecov(token=token)
