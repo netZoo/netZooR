be23892961b316543ce6914f254a2091a559a865
Marouen <marouenbg@users.noreply.github.com>
2021-08-17 18:17:50 -0400

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,6 +3,9 @@ library(netZooR)
 
 #download test data
 getwd()
+system('pwd')
+system('cd ..')
+system('pwd')
 system('cd ../inst/extdata/ && { curl -O https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/ppi.txt ; cd -; }')
 system('cd testthat/ && { curl -O https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/testDataset.RData ; cd -; }')
 
