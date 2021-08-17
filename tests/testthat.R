9bb665b3554f0f77f496050b0d9bbe0480714bfe
Marouen <marouenbg@users.noreply.github.com>
2021-08-17 15:45:10 -0400

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,5 +3,6 @@ library(netZooR)
 
 #download test data
 system('curl -o inst/extdata/ppi.txt https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/ppi.txt')
+system('curl -o tests/testthat/testDataset.RData https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/testDataset.RData')
 
 test_check("netZooR")
