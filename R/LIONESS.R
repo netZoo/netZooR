e3308360f37b2319943517ab817064f35673fe38
marouenbg <marouen.b.guebila@gmail.com>
2021-10-09 19:48:05 -0400

    fix lioness bugs

--- R/LIONESS.R
+++ R/LIONESS.R
@@ -168,7 +168,7 @@ lioness.py <- function(expr_file, motif_file=NULL, ppi_file=NULL, computing="cpu
 #' @examples
 #' data(pandaToyData)
 #' linonessRes <- lioness(pandaToyData$motif,
-#'     pandaToyData$expression[,1:10],pandaToyData$ppi,hamming=.1,progress=FALSE)
+#'     pandaToyData$expression[,1:3],pandaToyData$ppi,hamming=.1,progress=FALSE)
 #' @references
 #' Kuijjer, M.L., Tung, M., Yuan, G., Quackenbush, J. and Glass, K., 2015. 
 #' Estimating sample-specific regulatory networks. arXiv preprint arXiv:1505.06440.
