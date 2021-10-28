a45443d98129019d59cfa07f939d969df5a64f69
marouenbg <marouen.b.guebila@gmail.com>
2021-10-27 23:55:10 -0400

    add download file

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,7 @@ library(testthat)
 library(netZooR)
 
 #download test data
-system('cd testthat/ && { curl -O https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/ppi.txt ; cd -; }')
-system('cd testthat/ && { curl -O https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/testDataset.RData ; cd -; }')
+download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/ppi.txt','testthat/ppi.txt')
+download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/testDataset.RData','testthat/testDataset.RData')
 
 test_check("netZooR")
