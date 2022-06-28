5129e1cde1347896cfb55f4adb47da2d0105dccc
marouenbg <marouen.b.guebila@gmail.com>
2022-06-27 20:38:30 -0400

    update condor test

--- tests/testthat/test-condor.R
+++ tests/testthat/test-condor.R
@@ -21,7 +21,7 @@ test_that("Test CONDOR functions", {
   expect_error(condorCluster(condor.object,project = T),NA)
   
   # check modularity
-  expect_equal(condor.object$modularity,as.numeric(c("0.231404958677686","0.231404958677686")),tolerance=1e-7)
+  expect_equal(condor.object$modularity,c(0.231404958677686,0.231404958677686),tolerance=1e-7)
   
   # check community membership
   condor.red.memb <- condor.object$red.mem
