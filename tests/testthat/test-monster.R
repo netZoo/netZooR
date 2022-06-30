93275c7cc969c0ea5901cae315e836be8e87df3b
marouenbg <marouen.b.guebila@gmail.com>
2022-06-30 19:41:00 -0400

    add monster test

--- tests/testthat/test-monster.R
+++ tests/testthat/test-monster.R
@@ -15,15 +15,15 @@ test_that("MONSTER function works", {
   cc.net.1 <- suppressWarnings(monsterMonsterNI(yeast$motif,yeast$exp.cc[1:1000,1:20])) # suppress Warning messages glm.fit: fitted probabilities numerically 0 or 1 occurred
   cc.net.2 <- suppressWarnings(monsterMonsterNI(yeast$motif,yeast$exp.cc[1:1000,31:50]))
   # error:  Error in svd(X) : infinite or missing values in 'x' 
-  expect_equal(monster.transformation.matrix(cc.net.1, cc.net.2), monsterTM)
+  expect_equal(monsterTransformationMatrix(cc.net.1, cc.net.2), monsterTM)
   
-  # analyzes a bi-partite network by monster.transformation.matrix() function with method "kabsch".
+  # analyzes a bi-partite network by monsterTransformationMatrix() function with method "kabsch".
   # error in server
-  expect_equal(monster.transformation.matrix(cc.net.1, cc.net.2,method = "kabsch"), monsterTM_kabsch)
+  expect_equal(monsterTransformationMatrix(cc.net.1, cc.net.2,method = "kabsch"), monsterTM_kabsch)
   
-  # analyzes a bi-partite network by monster.transformation.matrix() function with method "L1".
+  # analyzes a bi-partite network by monsterTransformationMatrix() function with method "L1".
   # to do: error  Error: $ operator not defined for this S4 class 
-  monsterTM_L1 <- monster.transformation.matrix(cc.net.1, cc.net.2,method = "L1")
+  monsterTM_L1 <- monsterTransformationMatrix(cc.net.1, cc.net.2,method = "L1")
   
   data("monsterRes")
   
