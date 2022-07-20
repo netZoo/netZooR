4c63d99c7b743b83c8a64ddc294eafe5b68cc27c
marouenbg <marouen.b.guebila@gmail.com>
2022-07-20 00:46:11 -0400

    update test data

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,7 +2,7 @@ library(testthat)
 library(netZooR)
 
 #download test data
-download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/ppi_medium.txt','testthat/ppi_medium.txt')
+download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/example_datasets/ppi_medium.txt','testthat/ppi_medium.txt')
 download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/testDataset.RData','testthat/testDataset.RData')
 
 test_check("netZooR")
