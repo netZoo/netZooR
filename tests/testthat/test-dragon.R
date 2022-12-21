9d9fb9ffe3ab5d177784079c9f1098edc3a12bf1
katehoffshutta <43797774+katehoffshutta@users.noreply.github.com>
2022-12-20 21:10:09 -0500

    added export function and tests

--- tests/testthat/test-dragon.R
+++ tests/testthat/test-dragon.R
@@ -1,8 +1,6 @@
 # unit-tests for DRAGON
 context("test DRAGON helper functions")
 
-source("../../R/DRAGON.R")
-
 test_that("[DRAGON] scale() function yields expected results", {
   x = seq(1:100)
   scale_unbiased = scale(x) # default is bias=F
@@ -68,18 +66,64 @@ test_that("[DRAGON] get_shrunken_covariance_dragon() function returns the right 
 }
 )
 
-# test log likelihood function
-test_that("[DRAGON] Log likelihood function for estimation of kappa is correct",{
-  # log_lik_shrunken = function(kappa, p, lambda, rhos)
-  kappa = 10
-  p = 100
-  lambda = 0.1
-  rhos = runif(n = 100, min = -0.9, max = 0.9) # equation is valid for [-(1-lambda),(1-lambda)]
-  log_lik_shrunken(kappa = kappa, 
-                   p = p,
-                   lambda = lambda,
-                   rhos = rhos)
+test_that("[DRAGON] Risk grid in netZooR impl. matches risk grid from netZooPy impl.",
+{
+  toy_layer1 = matrix(c(1,2,3,1,5,12),nrow=3,byrow=T)
+  toy_layer2 = matrix(c(9,7,8),nrow=3,byrow=T)
+
+  res = dragon(layer1 = toy_layer1, layer2 = toy_layer2, pval = F)
+  # assert risk results are equal on toy data 
+  risk_py = read.csv("risk_grid_netzoopy.csv",header=F)
+  expect_equal(as.vector(t(res$risk_grid)), as.vector(as.matrix(risk_py)),tolerance = 1e-10)
 })
+
+# test dragon() exported function
+test_that("[DRAGON] dragon() exported function works on simulated data",
+{
+  # load dragon data simulated from python 
+  toy_layer1 = read.csv("dragon_layer1.csv",header=F)
+  toy_layer2 = read.csv("dragon_layer2.csv",header=F)
+  res = dragon(layer1 = toy_layer1, layer2 = toy_layer2, pval = F)
+  
+  # assert tuning parameter results are equal to that found with python 
+  python_lambda1 = 0.9074582855371163
+  python_lambda2 = 0.9132919090041549
+  
+  # These are the same 1e-5  
+  expect_equal(res$lambdas[1],python_lambda1,tol=1e-5)
+  expect_equal(res$lambdas[2],python_lambda2,tol=1e-5)
+  
+  # assert that output matrices are the same within this tolerance
+  py_cov = read.csv("dragon_python_cov.csv",header=F)
+  expect_equal(as.vector(res$cov),as.vector(as.matrix(py_cov)),tol=1e-5)
+  
+  # confirm the inversion matches to machine zero in R
+  expect_equal(as.vector(solve(res$cov)),as.vector(res$prec),tol=1e-15)
+  
+  # because of the propagation of differences that comes along with
+  # matrix inversion, we see bigger differences in the precision and ggm matrices
+  # tolerance of 1e-2
+  
+  py_prec = read.csv("dragon_python_prec.csv",header=F)
+  expect_equal(as.vector(res$prec),as.vector(as.matrix(py_prec)),tol=1e-2)
+  
+  py_parcor = read.csv("dragon_python_parcor.csv",header=F)
+  expect_equal(as.vector(res$ggm),as.vector(as.matrix(py_parcor)),tol=1e-2)
+
+})
+
+# test log likelihood function
+# test_that("[DRAGON] Log likelihood function for estimation of kappa is correct",{
+#   # log_lik_shrunken = function(kappa, p, lambda, rhos)
+#   kappa = 10
+#   p = 100
+#   lambda = 0.1
+#   rhos = runif(n = 100, min = -0.9, max = 0.9) # equation is valid for [-(1-lambda),(1-lambda)]
+#   log_lik_shrunken(kappa = kappa, 
+#                    p = p,
+#                    lambda = lambda,
+#                    rhos = rhos)
+# })
 
 #testing format
 #test_that(,{})
