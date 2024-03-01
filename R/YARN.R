55352550c0bd4d0c1a0457edfe1da9d5810dc375
marouenbg <marouen.b.guebila@gmail.com>
2024-03-01 16:59:01 -0500

    update yarn

--- R/YARN.R
+++ R/YARN.R
@@ -20,8 +20,10 @@
 #' @importClassesFrom Biobase ExpressionSet
 #'
 #' @examples
-#' download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/yarn/bladder.rdata',destfile='../data/bladder.rdata')
-#' download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/yarn/skin',destfile='../data/skin.rdata')
+#' download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/yarn/bladder.rdata',destfile='data/bladder.rdata')
+#' system(print('hello'))
+#' system('ls')
+#' download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/yarn/skin',destfile='data/skin.rdata')
 #' data(skin)
 #' # subsetting and changing column name just for a silly example
 #' skin <- skin[1:10,]
