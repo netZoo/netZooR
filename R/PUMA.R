c5d2f8779a564d72eadfe622754fbcbc119a7726
Tara Eicher <taraeicher@yahoo.com>
2024-09-23 16:11:58 -0400

    Fixed PUMA example with pandaObj call

--- R/PUMA.R
+++ R/PUMA.R
@@ -431,3 +431,6 @@ prepResult <- function(zScale, output, regulatoryNetwork, geneCoreg, tfCoopNetwo
   pandaObj(regNet=regulatoryNetwork, coregNet=geneCoreg, coopNet=tfCoopNetwork, numGenes=numGenes, numTFs=numTFs, numEdges=numEdges)
 }
 
+# Define a new class called pandaObj.
+pandaObj <- setClass("panda", slots=c("regNet","coregNet","coopNet","numGenes","numTFs","numEdges"))
+setMethod("show","panda",function(object){print.panda(object)})
