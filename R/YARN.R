350744a96173d8ea5c6069cba07ff56b7deb1003
Tara Eicher <taraeicher@yahoo.com>
2024-09-27 23:08:20 -0400

    Modified line widths to address build note

--- R/YARN.R
+++ R/YARN.R
@@ -20,8 +20,11 @@
 #' @importClassesFrom Biobase ExpressionSet
 #'
 #' @examples
-#' download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/yarn/bladder.rdata',destfile='netZooR/data/bladder.rdata')
-#' download.file('https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/yarn/skin.rdata',destfile='netZooR/data/skin.rdata')
+#' u <- 'https://netzoo.s3.us-east-2.amazonaws.com/netZooR/unittest_datasets/'
+#' bladder <- paste0(u, 'yarn/bladder.rdata')
+#' skin <- paste0(u, 'yarn/skin.rdata')
+#' download.file(bladder, destfile='netZooR/data/bladder.rdata')
+#' download.file(skin, destfile='netZooR/data/skin.rdata')
 #' data(skin)
 #' # subsetting and changing column name just for a silly example
 #' skin <- skin[1:10,]
